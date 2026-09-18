c82adce81cb96780a92b708442721d0154c328c0
starfruit36 <leminhngoc3669420@gmail.com>
2026-09-17 22:37:29 -0600

    add some operation, and todo

--- haskell/app/Main.hs
+++ haskell/app/Main.hs
@@ -11,7 +11,20 @@ xor a b
     |a == b = B0
     |otherwise = B1
 
-countOne :: [Bit] -> Integer
+not :: Bit -> Bit
+not B0 = B1
+not B1 = B0
+
+addBit :: Bit -> Bit -> Bit
+addBit B0 x  = x
+addBit B1 B0 = B1
+addBit B1 B1 = B0
+
+mulBit :: Bit -> Bit -> Bit
+mulBit B1 B1 = B1
+mulBit _  _  = B0
+
+countOne :: [Bit] -> Integer -- find weight as well
 countOne [] = 0
 countOne (B1:xs) = 1 + countOne xs
 countOne (B0:xs) = countOne xs
@@ -27,9 +40,21 @@ majority xs
     |countZero xs > countOne xs = B0
     |otherwise = Nothing
 
-findRedundancy :: Integer -> Integer -> Integer
+findRedundancy :: Integer -> Integer -> Integer --2^r >= n + 1, n = m + p
 findRedundancy m p
-    |2^p >= m + p + 1 = p
+    |2^p >= m + p + 1 = p   
     |otherwise = findRedundancy m (p+1)
 
--- hamming :: Integer -> Integer -> [Bit]
+{-Todo: SEC edition
+ matrix mul use list comprehension.
+In fact, maybe matrix data, though pretty useless with [[Bit]]
+matrix augmentation needed using list operation
+construct hamming matrix k*n, where position 2^i = p diagonal of I, the rest are lexical order.
+construct the generator matrix I|P (for consistency, the hamming matrix might be A|I*P,so use that A to derive G using I|A^TP)
+transpose needed, using list comprehension
+construct code word, using xG = c, for all x span 2^k
+maybe implement bsc ish error simulation, not too sure
+map a mapping on what to do (aka, the inverse of hamming matrix construction to find index using symptom and flip). 
+So prlly binary expansion needed, bin to int and int to bin
+-}
+
